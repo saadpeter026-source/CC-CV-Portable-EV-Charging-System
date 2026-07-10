ccda0da1aa453670180f17d1a2dbeeb1d53ae3bb
saadpeter026-source <saadpeter026@gmail.com>
2026-07-09 20:06:11 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -45,3 +45,75 @@ batteryTemp = logs.get('EV Battery Temperature During Charging');
 junctionTemp = logs.get('IGBT Junction Temperature During Charging');
 caseTemp = logs.get('IGBT Case Temperature During Charging');
 heatsinkTemp = logs.get('IGBT Heatsink Temperature During Charging');
+
+
+%% Plot Results
+
+% EV Battery Charging Voltage
+figure;
+plot(evVoltage.Values.Time, evVoltage.Values.Data, 'LineWidth', 2);
+title('EV Battery Charging Voltage');
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+grid on;
+
+% EV Battery Charging Current
+figure;
+plot(evCurrent.Values.Time, evCurrent.Values.Data, 'LineWidth', 2);
+title('EV Battery Charging Current');
+xlabel('Time (s)');
+ylabel('Current (A)');
+grid on;
+
+% EV Battery State of Charge
+figure;
+plot(evSOC.Values.Time, evSOC.Values.Data, 'LineWidth', 2);
+title('EV Battery State of Charge');
+xlabel('Time (s)');
+ylabel('State of Charge (%)');
+grid on;
+
+% Portable Battery State of Charge
+figure;
+plot(portableSOC.Values.Time, portableSOC.Values.Data, 'LineWidth', 2);
+title('Portable Battery State of Charge');
+xlabel('Time (s)');
+ylabel('State of Charge (%)');
+grid on;
+
+% EV Battery Temperature
+figure;
+plot(batteryTemp.Values.Time, batteryTemp.Values.Data, 'LineWidth', 2);
+title('EV Battery Temperature');
+xlabel('Time (s)');
+ylabel('Temperature (°C)');
+grid on;
+
+% IGBT Temperatures
+figure;
+plot(junctionTemp.Values.Time, junctionTemp.Values.Data, 'LineWidth', 2);
+hold on;
+plot(caseTemp.Values.Time, caseTemp.Values.Data, 'LineWidth', 2);
+plot(heatsinkTemp.Values.Time, heatsinkTemp.Values.Data, 'LineWidth', 2);
+hold off;
+
+title('IGBT Temperatures');
+xlabel('Time (s)');
+ylabel('Temperature (°C)');
+legend('Junction','Case','Heatsink','Location','best');
+grid on;
+
+% Efficiency
+figure;
+plot(eff.Values.Time, eff.Values.Data, 'LineWidth', 2);
+title('Charging Efficiency');
+xlabel('Time (s)');
+ylabel('Efficiency (%)');
+grid on;
+
+disp('Simulation completed successfully.');
+
+
+
+
+
